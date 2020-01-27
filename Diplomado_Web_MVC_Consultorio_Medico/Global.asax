99afb64f42496e3c5446ce1a04d42fa311791d95
Jonathan Lorenzo Contrera <jonathanlorenzoc_rt@outlook.es>
2020-01-27 12:17:29 -0400

    Add project files.

---
+++ Diplomado_Web_MVC_Consultorio_Medico/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Diplomado_Web_MVC_Consultorio_Medico.MvcApplication" Language="C#" %>
---
+++ Diplomado_Web_MVC_Consultorio_Medico/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Diplomado_Web_MVC_Consultorio_Medico.Startup))]
+namespace Diplomado_Web_MVC_Consultorio_Medico
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
