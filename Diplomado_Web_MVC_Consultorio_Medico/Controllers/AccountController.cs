9722d8b805da3fb0a7d937be3a21202b175c5786
Jonathan Lorenzo Contrera <jonathanlorenzoc_rt@outlook.es>
2020-01-28 16:14:44 -0400

    Actualizacion

--- Diplomado_Web_MVC_Consultorio_Medico/Controllers/AccountController.cs
+++ Diplomado_Web_MVC_Consultorio_Medico/Controllers/AccountController.cs
@@ -22,7 +22,7 @@ namespace Diplomado_Web_MVC_Consultorio_Medico.Controllers
         {
         }
 
-        public AccountController(ApplicationUserManager userManager, ApplicationSignInManager signInManager )
+        public AccountController(ApplicationUserManager userManager, ApplicationSignInManager signInManager)
         {
             UserManager = userManager;
             SignInManager = signInManager;
@@ -34,9 +34,9 @@ namespace Diplomado_Web_MVC_Consultorio_Medico.Controllers
             {
                 return _signInManager ?? HttpContext.GetOwinContext().Get<ApplicationSignInManager>();
             }
-            private set 
-            { 
-                _signInManager = value; 
+            private set
+            {
+                _signInManager = value;
             }
         }
 
@@ -120,7 +120,7 @@ namespace Diplomado_Web_MVC_Consultorio_Medico.Controllers
             // If a user enters incorrect codes for a specified amount of time then the user account 
             // will be locked out for a specified amount of time. 
             // You can configure the account lockout settings in IdentityConfig
-            var result = await SignInManager.TwoFactorSignInAsync(model.Provider, model.Code, isPersistent:  model.RememberMe, rememberBrowser: model.RememberBrowser);
+            var result = await SignInManager.TwoFactorSignInAsync(model.Provider, model.Code, isPersistent: model.RememberMe, rememberBrowser: model.RememberBrowser);
             switch (result)
             {
                 case SignInStatus.Success:
@@ -151,12 +151,21 @@ namespace Diplomado_Web_MVC_Consultorio_Medico.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new ApplicationUser
+                {
+                    Nombre = model.Nombre,
+                    Apellido = model.Apellido,
+                    Direccion = model.Direccion,
+                    Fecha_Nacimiento = model.Fecha_Nacimiento,
+                    Identificacion = model.Identificacion,
+                    UserName = model.Email,
+                    Email = model.Email
+                };
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
-                    await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
-                    
+                    await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
+
                     // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
                     // Send an email with this link
                     // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
